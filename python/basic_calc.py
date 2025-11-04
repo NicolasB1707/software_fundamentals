c37de82527405494f81c5698c56fb99889f5d6ee
Nicolas <tucorreo@gmail.com>
2025-11-03 19:32:33 -0500

    python script for a basic calculator

---
+++ python/basic_calc.py
@@ -0,0 +1,27 @@
+#Algoritm description: Basic calc v1
+#Get two numbers 
+#Add, sub, mul, div
+#Print results
+
+
+	#2. Initialize vars and/or const
+	# inputs
+num1 =10
+num2 = 5
+	#3.processes
+add = num1 + num2
+subs = num1 - num2
+mult = num1 * num2
+div = num1 / num2
+	#4. Outputs without f-strings
+print("addition: ",add,type(add))
+print("subtraction:",subs,type(subs))
+print("Multipication:",mult,type(mult))
+print("Division:",div,type(div))
+	
+    #4. Outputs with f-strings
+print( f"addition: {add}")    
+print( f"subtraction: {subs}")
+print( f"Multipication: {mult}")
+print( f"Division: {div}")
+
